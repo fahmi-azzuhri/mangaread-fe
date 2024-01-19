bc7a90b36298250ea961668e5d8a7e900241baff
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-01-19 23:39:55 +0700

    initiate folder structure

---
+++ src/components/header/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>H</div>;
+};
+
+export default Header;
---
+++ src/components/tabel/tabel.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Tabel = () => {
+  return <div>Tabel</div>;
+};
