1c201cd171747e96a8c3850132b52a1f36f429c3
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-01-19 23:44:39 +0700

    router setup and create header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./headerStyle.css";
 
-const Header = () => {
-  return <div>H</div>;
+const Heading = () => {
+  return (
+    <header className="header-container">
+      <div className="header-title">Mangalists</div>
+      <Link to={"/input"} className="button-wrapper">
+        <div>
+          <img src="/plus.svg" />
+          Add Data
+        </div>
+      </Link>
+    </header>
+  );
 };
 
-export default Header;
+export default Heading;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Edit from "./components/edit/edit";
+import Tabel from "./components/tabel/tabel";
+import Input from "./components/input/input";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Tabel /> },
+      {
+        path: "edit/:editId",
+        element: <Edit />,
+      },
+      {
+        path: "input",
+        element: <Input />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
